f54bd02c35cb8b2f630c40d2db7b6f85bdc98093
loughranico <nicholas.loughran@lisdatasolutions.com>
2022-10-05 16:54:13 +0200

    Changed size for testing

--- 5. Delivery Optimization/main.py
+++ 5. Delivery Optimization/main.py
@@ -28,12 +28,12 @@ for i in range(4):
 env.render()'''
 
 
-env = DeliveryEnvironment(n_stops = 100,method = "distance")
+env = DeliveryEnvironment(n_stops = 10,method = "distance")
 agent = DeliveryQAgent(env.observation_space,env.action_space)
 
 #e,a,env_min = run_n_episodes(env,agent,"training_100_stops.gif")
 
-run_n_episodes(env,agent,"training_100_stops.gif",n_episodes = 2000)
+run_n_episodes(env,agent,"training_10_stops.gif",n_episodes = 2000)
 
 env.render()
 #env_min.render()
